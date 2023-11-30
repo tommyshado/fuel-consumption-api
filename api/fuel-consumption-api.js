eb6f3943e6998585a983fb732f1a5d89a532fce1
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-11-30 13:19:10 +0200

    created functionality to display vehicles

--- api/fuel-consumption-api.js
+++ api/fuel-consumption-api.js
@@ -4,7 +4,6 @@ export default function FuelConsumptionAPI(fuelConsumption) {
 
     async function addVehicle(req, res) {
         const {description, regNumber} = req.body;
-        console.log(req.body);
 
         const result  = await fuelConsumption.addVehicle({description, regNumber});
         res.json(result);
@@ -22,7 +21,7 @@ export default function FuelConsumptionAPI(fuelConsumption) {
 
     async function vehicle(req, res) {
 
-        const {id} = req.params;
+        const {id} = req.query;
 
         const vehicle = await fuelConsumption.vehicle(id)
         res.json({
@@ -34,7 +33,6 @@ export default function FuelConsumptionAPI(fuelConsumption) {
     async function refuel(req, res) {
         
         const { vehicleId, liters, amount, distance, filledUp } = req.body;
-        console.log(req.body);
         
         const status = await fuelConsumption.refuel(vehicleId, liters, amount, distance, filledUp)
         res.json(status);
---
+++ public/client.js
@@ -0,0 +1,23 @@
+
+// Template source
+const templateSource = document.querySelector(".template").innerHTML;
+let vehicles = document.querySelector(".vehicles");
+const template = Handlebars.compile(templateSource);
+
+
+
+// Functionality to display vehicles
+const vehiclesURL = () => {
+    const url = "http://localhost:3000/api/vehicles";
+    return axios.get(url);
+};
+
+const displayVehicles = () => {
+    vehiclesURL().then(results => {
+        vehicles.innerHTML = template(results.data.data);
+    })
+};
+
+displayVehicles();
+
+// Functionality to add a vehicle in vehicles.html
